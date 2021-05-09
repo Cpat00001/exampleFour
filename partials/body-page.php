08b43194da9f3b30021f3c8ea6fcb4cc321041b7
Pat <p171982@gmail.com>
2021-05-09 19:54:22 +0100

    css skew

--- partials/body-page.php
+++ partials/body-page.php
@@ -1,13 +1,23 @@
 
- <h5>Page Body</h5>
 <div class="body_wrapper">
     <div class="b_one">
         <button id="btn_1">Click to test</button>
         <p id="show_txt"></p>
     </div>
     <div class="b_two">
-        <div class="skewed_one">
-            <h5>Skewed Div</h5>
-        </div>
+        <h5>B_Two</h5>
+        <!-- <div class="skewed_one">
+            <div class="not_skewed_container">
+                <div class="not_skewed_one">
+                    <h5 id="center_txt">Normal div</h5>
+                </div>
+                <div class="not_skewed_two">
+                    <h5 id="center_txt">Normal div</h5>
+                </div>
+            </div>
+        </div> -->
+    </div>
+    <div class="b_three">
+        <h5>B_Three</h5>
     </div>
 </div>
