f4ee2c017b5c99cd728860dd0e0f56655d6446c5
Pat <p171982@gmail.com>
2021-05-19 22:13:21 +0100

    changes

--- partials/letters.php
+++ partials/letters.php
@@ -1,6 +1,5 @@
 <div class="letters-container">
     <div class="letters-show">
         <h1 id="letters-txt"><span>LETTERS</span></h1>
-        <img src="https://images.pexels.com/photos/4116231/pexels-photo-4116231.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
     </div>
 </div>
