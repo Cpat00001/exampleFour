b08ffda0549d4a02ec409baef7b350c2dfcd10e1
Pat <p171982@gmail.com>
2021-05-21 21:30:14 +0100

    grid news/media

--- partials/body-page.php
+++ partials/body-page.php
@@ -5,7 +5,20 @@
         <p id="show_txt"></p>
     </div>
     <div class="b_two">
-        <h5>B_Two</h5>
+        <div class="b_two_left">
+            <div class="b_two_left_head title">
+                Header
+            </div>
+            <div class="b_two_left_body">Body</div>
+        </div>
+        <div class="b_two_middle">
+            <div class="b_two_middle_head title">Header</div>
+            <div class="b_two_middle_body">Some text here</div>
+        </div>
+        <div class="b_two_right">
+            <div class="b_two_right_head title">Right Header Photo</div>
+            <div class="b_two_right_body">Right Body Title</div>
+        </div>
     </div>
     <div class="b_three">
         <h5>B_Three</h5>
--- partials/letters.php
+++ partials/letters.php
@@ -1,5 +1,5 @@
 <div class="letters-container">
     <div class="letters-show">
-        <h1 id="letters-txt"><span>LETTERS</span></h1>
+        <h1 id="letters-txt"><span>REPAIRS!</span></h1>
     </div>
 </div>
